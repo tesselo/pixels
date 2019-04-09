35896dda774f6e88fede253666d665cc601960ae
Daniel Wiesmann <daniel@tesselo.com>
2019-04-09 16:44:41 +0100

    Added input args to lambda handler.

--- pixels/app.py
+++ pixels/app.py
@@ -2,38 +2,46 @@ import copy
 import json
 import zipfile
 from io import BytesIO
+import logging
 
 from pixels import const, scihub, search
 from flask import Flask, send_file, request
 
 app = Flask(__name__)
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def handler():
+def pixels(event=None, context=None):
     """
     AWS Lambda ready handler for the pixels package.
 
     The input event is a JSON configuration for a pixels request.
     """
+    print('event', event)
+    print('context', context)
+    data = request.get_json()
+    print('data', data)
+
     if request.method == 'GET':
         return "Use POST to query pixels!", 200
 
-    event = request.get_json()
     # Get extract custom handler arguments.
-    search_only = event.pop('search_only', False)
-    composite = event.pop('composite', False)
-    latest_pixel = event.pop('latest_pixel', False)
-    color = event.pop('color', False)
-    bands = event.pop('bands', [])
-    scale = event.pop('scale', 10)
+    search_only = data.pop('search_only', False)
+    composite = data.pop('composite', False)
+    latest_pixel = data.pop('latest_pixel', False)
+    color = data.pop('color', False)
+    bands = data.pop('bands', [])
+    scale = data.pop('scale', 10)
 
     if not composite and not latest_pixel:
         # TODO: Allow getting multiple stacks as "raw" scenes data.
         raise ValueError('Choose either latest pixel or composite mode.')
 
     # Sanity checks.
-    if composite and event.get('platform', None) == const.PLATFORM_SENTINEL_1:
+    if composite and data.get('platform', None) == const.PLATFORM_SENTINEL_1:
         raise ValueError('Cannot compute composite for Sentinel 1.')
 
     # For composite, we will require all bands to be retrieved.
@@ -41,8 +49,8 @@ def handler():
         print('Requesting all Sentinel-2 bands for composite mode.')
         bands = const.SENTINEL_2_BANDS
 
-    # Store original event and possible overrides.
-    config_log = copy.deepcopy(event)
+    # Store original data and possible overrides.
+    config_log = copy.deepcopy(data)
     config_log['search_only'] = search_only
     config_log['composite'] = composite
     config_log['latest_pixel'] = latest_pixel
@@ -51,8 +59,8 @@ def handler():
     config_log['scale'] = scale
 
     # Query available scenery.
-    print('Querying data on the ESA SciHub.', event)
-    query_result = search.search(**event)
+    print('Querying data on the ESA SciHub.', data)
+    query_result = search.search(**data)
 
     # Return only search query if requested.
     if search_only:
@@ -61,14 +69,14 @@ def handler():
     # Get pixels.
     if latest_pixel:
         print('Getting latest pixels stack.')
-        stack = scihub.latest_pixel(event['geom'], query_result, scale=scale, bands=bands, as_file=True)
+        stack = scihub.latest_pixel(data['geom'], query_result, scale=scale, bands=bands, as_file=True)
     else:
         #### HACK TODO REMOVE
-        #BD2 = scihub.get_pixels(event['geom'], query_result[0], scale=scale, bands=['B02'])['B02']
+        #BD2 = scihub.get_pixels(data['geom'], query_result[0], scale=scale, bands=['B02'])['B02']
 
         for entry in query_result:
             print('Getting scene pixels for', entry['prefix'])
-            entry['pixels'] = scihub.get_pixels(event['geom'], entry, scale=scale, bands=bands)
+            entry['pixels'] = scihub.get_pixels(data['geom'], entry, scale=scale, bands=bands)
             #entry['pixels'] = {key: BD2 for key in const.SENTINEL_2_BANDS}
 
         if composite:
@@ -79,7 +87,7 @@ def handler():
     # Convert to RGB color tif.
     if color:
         print('Computing RGB image from stack.')
-        if event.get('platform', None) == const.PLATFORM_SENTINEL_1:
+        if data.get('platform', None) == const.PLATFORM_SENTINEL_1:
             stack['RGB'] = scihub.s1_color(stack, as_file=True)
         else:
             stack['RGB'] = scihub.s2_color(stack, as_file=True)
