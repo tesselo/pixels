2865ad3d5c59077f04c40ae4e61c9a674085734c
Daniel Wiesmann <daniel@tesselo.com>
2019-04-26 12:37:28 +0100

    Changed test scripts to adapt to new endpoints.

--- tests/test_training_collection.py
+++ tests/test_training_collection.py
@@ -10,7 +10,7 @@ import requests
 from dateutil import parser
 from shapely.geometry import shape
 
-bands = ["B04", "B08"]
+bands = ["B01"]
 
 config = {
     "geom": {
@@ -27,8 +27,8 @@ config = {
             ]]
         },
     },
-    'start': '2018-01-01',
-    'end': '2018-01-18',
+    'start': '2018-08-01',
+    'end': '2018-08-18',
     'interval': 'weeks',
     "platform": 'Sentinel-2',
     "product_type": 'S2MSI1C',
@@ -36,12 +36,13 @@ config = {
     "search_only": False,
     "composite": False,
     "latest_pixel": True,
-    "color": True,
-    "format": 'PNG',
+    "color": False,
+    "format": 'ZIP',
     "delay": True,
+    "scale": 20,
     "bands": [],
     "clip_to_geom": True,
-    "interval": "weekly",
+    "interval": "weeks",
     "interval_step": 1,
 }
 
@@ -51,68 +52,34 @@ shape_config = {
 }
 
 
-
-def funk(config):
-    start = parser.parse(config['start'])
-    end = parser.parse(config['end'])
-    print(start, end, start < end)
-
-    assert start <= end
-
-    delta = datetime.timedelta(**{config.pop('interval'): 1})
-    here_start = start
-    here_end = start + delta
-    counter = 1
-    results = []
-    while here_end <= end:
-        counter += 1
-        here_end += delta
-        here_start += delta
-        print(here_start, here_end)
-        config.update({
-            'start': str(here_start.date()),
-            'end': str(here_end.date()),
-        })
-        response = requests.post(endpoint, json=config)
-        print(response.content)
-        result = copy.deepcopy(config)
-        result.update(response.json())
-        results.append(result)
-
-    return results
-endpoint = 'https://pixels.tesselo.com/data?key=b0ef31c1be11bede35c874ca6c5e5361e95598df'
-# endpoint = 'http://127.0.0.1:5000/data?key=829c0f290b9f0f0d49fd2501e5792f8413305535'
+# endpoint = 'https://pixels.tesselo.com/timeseries?key=b0ef31c1be11bede35c874ca6c5e5361e95598df'
+endpoint = 'http://127.0.0.1:5000/timeseries?key=829c0f290b9f0f0d49fd2501e5792f8413305535'
 # endpoint = 'https://devpixels.tesselo.com/data?key=78f300a8965e04f111e2a738a9b1cbc4f6a8bc55'
 
-results = []
-with fiona.open('/media/tam/rhino/work/projects/tesselo/projects/celpa/celpa_national/new_data/areas teste_28set2018.shp', 'r', encoding='latin-1') as src:
-    for feat in src:
-        config['geom'] = feat
-        print(src.crs)
-        config['geom']['crs'] = src.crs['init']
-        result = funk(config)
-        result['feature_identifier'] = feat['id']
-        results.append()
-        break
+result = requests.post(endpoint, json=config)
 
+print(result.status_code)
+pprint.pprint(result.json())
+
+# 'http://127.0.0.1:5000/timeseries/be190e0e-eeb6-4677-b7bd-07ff974baeb2/data.zip?key=829c0f290b9f0f0d49fd2501e5792f8413305535'
 # pprint.pprint(src[1])
 
 #
 # result = funk(config)
-result = results[0]
-
-result_npz = [numpy.load(io.BytesIO(requests.get(dat['url']).content)) for dat in result]
-
-result_np = [{band: dat[band].ravel() for band in (bands + ['config'])} for dat in result_npz]
-
-result_np = []
-for dat in result_npz:
-    tmp = {band: dat[band].ravel() for band in bands}
-    tmp['start'] = dat['config'].item()['start']
-    tmp['end'] = dat['config'].item()['end']
-    result_np.append(tmp)
-
-result_pd = [pandas.DataFrame(dat) for dat in result_np]
-
-
-print(result_pd)
+# result = results[0]
+#
+# result_npz = [numpy.load(io.BytesIO(requests.get(dat['url']).content)) for dat in result]
+#
+# result_np = [{band: dat[band].ravel() for band in (bands + ['config'])} for dat in result_npz]
+#
+# result_np = []
+# for dat in result_npz:
+#     tmp = {band: dat[band].ravel() for band in bands}
+#     tmp['start'] = dat['config'].item()['start']
+#     tmp['end'] = dat['config'].item()['end']
+#     result_np.append(tmp)
+#
+# result_pd = [pandas.DataFrame(dat) for dat in result_np]
+#
+#
+# print(result_pd)
