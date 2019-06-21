cb4bbf760e90c3506ef3779e50da4c20f56d82b4
Daniel Wiesmann <daniel@tesselo.com>
2019-06-21 16:47:38 +0100

    Moved image rendering pieces into separate module and created functions for them.

--- app/app.py
+++ app/app.py
@@ -10,14 +10,16 @@ from io import BytesIO
 
 import boto3
 import numpy
-import rasterio
 from dateutil import parser
 from flask import Flask, Response, has_request_context, jsonify, redirect, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
-from PIL import Image, ImageDraw, ImageEnhance
+from PIL import Image, ImageDraw
+from rasterio import Affine
+from rasterio.io import MemoryFile
 from zappa.async import task
 
-from pixels import algebra, const, core, utils, wmts
+from app import utils as app_utils
+from pixels import const, core, utils, wmts
 from pixels.exceptions import PixelsFailed
 
 # Flask setup
@@ -424,173 +426,81 @@ def timeseries_result(tag):
     return redirect(url)
 
 
-def hex_to_rgba(value, alpha=255):
-    """
-    Converts a HEX color string to a RGBA 4-tuple.
-    """
-    if value is None:
-        return [None, None, None]
-
-    value = value.lstrip('#')
-
-    # Check length and input string property
-    if len(value) not in [1, 2, 3, 6] or not value.isalnum():
-        raise PixelsFailed('Invalid color, could not convert hex to rgb.')
-
-    # Repeat values for shortened input
-    value = (value * 6)[:6]
-
-    # Convert to rgb
-    return int(value[0:2], 16), int(value[2:4], 16), int(value[4:6], 16), alpha
-
-
-def rescale_to_channel_range(data, dfrom, dto, dover=None):
-    """
-    Rescales an array to the color interval provided. Assumes that the data is normalized.
-    This is used as a helper function for continuous colormaps.
-    """
-    # If the interval is zero dimensional, return constant array.
-    if dfrom == dto:
-        return numpy.ones(data.shape) * dfrom
-
-    if dover is None:
-        # Invert data going from smaller to larger if origin color is bigger
-        # than target color.
-        if dto < dfrom:
-            data = 1 - data
-        return data * abs(dto - dfrom) + min(dto, dfrom)
-    else:
-        # Divide data in upper and lower half.
-        lower_half = data < 0.5
-        # Recursive calls to scaling upper and lower half separately.
-        data[lower_half] = rescale_to_channel_range(data[lower_half] * 2, dfrom, dover)
-        data[numpy.logical_not(lower_half)] = rescale_to_channel_range((data[numpy.logical_not(lower_half)] - 0.5) * 2, dover, dto)
-
-    return data
-
-
-@app.route('/composite/<projectid>/<int:z>/<int:x>/<int:y>.png', methods=['GET'])
+@app.route('/composite/<projectid>/<int:z>/<int:x>/<int:y>.<frmt>', methods=['GET'])
 @token_required
-def composite(projectid, z, x, y):
+def composite(projectid, z, x, y, frmt='png'):
     """
     Show tiles from TMS creation.
     """
-    return_empty = False
-    sentinel_1 = False
-    # Look for a json string in data argument.
+    # Get formula.
     formula = request.args.get('formula', None)
-    if formula and not return_empty:
-        parser = algebra.FormulaParser()
-        data = {}
-        for band in const.SENTINEL_2_BANDS:
-            if band in formula:
-                try:
-                    with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!{}.tif'.format(const.BUCKET, projectid, z, x, y, band)) as rst:
-                        data[band] = rst.read(1).T.astype('float')
-                except rasterio.errors.RasterioIOError:
-                    return_empty = True
+    # Sanity checks.
+    if frmt not in ['png', 'tif']:
+        raise PixelsFailed('Format {} not recognized. Use png or tif.'.format(frmt))
+    if frmt == 'tif' and not formula:
+        raise PixelsFailed('Format TIF is only available for formula requests.')
 
-        if not return_empty:
-            index = parser.evaluate(data, formula)
-
-            dmin = float(request.args.get('min', -1))
-            dmax = float(request.args.get('max', 1))
-
-            if dmax == dmin:
-                norm = index == dmin
-            else:
-                norm = (index - dmin) / (dmax - dmin)
-
-            color_from = hex_to_rgba(request.args.get('from', '00'))
-            color_to = hex_to_rgba(request.args.get('to', 'FF'))
-            color_over = hex_to_rgba(request.args.get('over', None))
-
-            red = rescale_to_channel_range(norm.copy(), color_from[0], color_to[0], color_over[0])
-            green = rescale_to_channel_range(norm.copy(), color_from[1], color_to[1], color_over[1])
-            blue = rescale_to_channel_range(norm.copy(), color_from[2], color_to[2], color_over[2])
-
-            # Compute alpha channel from mask if available.
-            alpha = numpy.all([dat != 0 for dat in data.values()], axis=0) * 255
-
-            pixpix = numpy.array([red, green, blue, alpha], dtype='uint8')
+    if formula:
+        data = app_utils.get_s2_formula_pixels(
+            projectid,
+            z, x, y,
+            formula,
+            request.args.get('from', '00'),
+            request.args.get('to', 'FF'),
+            request.args.get('over', None),
+            float(request.args.get('min', -1)),
+            float(request.args.get('max', 1)),
+            frmt,
+        )
+        mode = 'formula'
     else:
-        try:
-            with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!B04.tif'.format(const.BUCKET, projectid, z, x, y)) as rst:
-                red = rst.read(1)
-            with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!B03.tif'.format(const.BUCKET, projectid, z, x, y)) as rst:
-                green = rst.read(1)
-            with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!B02.tif'.format(const.BUCKET, projectid, z, x, y)) as rst:
-                blue = rst.read(1)
-            mask = numpy.all((red != 0, blue != 0, green != 0), axis=0).T * 255
-        except rasterio.errors.RasterioIOError:
-            try:
-                with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!VV.tif'.format(const.BUCKET, projectid, z, x, y)) as rst:
-                    red = rst.read(1)
-                with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!VH.tif'.format(const.BUCKET, projectid, z, x, y)) as rst:
-                    green = rst.read(1)
-            except rasterio.errors.RasterioIOError:
-                try:
-                    with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!HH.tif'.format(const.BUCKET, projectid, z, x, y)) as rst:
-                        red = rst.read(1)
-                    with rasterio.open('zip+s3://{}/{}/tiles/{}/{}/{}/pixels.zip!HV.tif'.format(const.BUCKET, projectid, z, x, y)) as rst:
-                        green = rst.read(1)
-                except:
-                    return_empty = True
-            if not return_empty:
-                mask = numpy.all((red != 0, green != 0), axis=0).T * 255
+        # S2 Pixels.
+        data = app_utils.get_s2_rgb_pixels(projectid, z, x, y)
+        mode = 'S2'
+        if not data:
+            # S1 Pixels.
+            data = app_utils.get_s1_rgb_pixels(projectid, z, x, y)
+            mode = 'S1'
 
-                # Transform the data to provide an interpretable visual result.
-                red = numpy.log(red)
-                green = numpy.log(green)
-
-                red = (red / 7) * const.SENTINEL_2_RGB_CLIPPER
-                green = (green / 7) * const.SENTINEL_2_RGB_CLIPPER
-                blue = (red / green) * const.SENTINEL_2_RGB_CLIPPER / 2
-
-                sentinel_1 = True
-
-        if not return_empty:
+    if data is None:
+        output = app_utils.get_empty_image(z)
+    else:
+        if formula:
+            if frmt == 'tif':
+                output = MemoryFile()
+                scale = utils.tile_scale(z)
+                tbounds = utils.tile_bounds(z, x, y)
+                creation_args = {
+                    'driver': 'GTiff',
+                    'dtype': 'float',
+                    'nodata': None,
+                    'width': 256,
+                    'height': 256,
+                    'count': 1,
+                    'crs': 'epsg:3857',
+                    'transform': Affine(scale, 0.0, tbounds[0], 0.0, -scale, tbounds[3]),
+                }
+                with output.open(**creation_args) as dst:
+                    dst.write(data.reshape(1, 256, 256))
+                output.seek(0)
+            else:
+                pixpix = numpy.array(data, dtype='uint8')
+                output = app_utils.get_image_from_pixels(pixpix, enhance=mode == 'S2')
+        else:
             pixpix = numpy.array(
                 [
-                    numpy.clip(red, 0, const.SENTINEL_2_RGB_CLIPPER).T * 255 / const.SENTINEL_2_RGB_CLIPPER,
-                    numpy.clip(green, 0, const.SENTINEL_2_RGB_CLIPPER).T * 255 / const.SENTINEL_2_RGB_CLIPPER,
-                    numpy.clip(blue, 0, const.SENTINEL_2_RGB_CLIPPER).T * 255 / const.SENTINEL_2_RGB_CLIPPER,
-                    mask,
+                    numpy.clip(data[0], 0, const.SENTINEL_2_RGB_CLIPPER).T * 255 / const.SENTINEL_2_RGB_CLIPPER,
+                    numpy.clip(data[1], 0, const.SENTINEL_2_RGB_CLIPPER).T * 255 / const.SENTINEL_2_RGB_CLIPPER,
+                    numpy.clip(data[2], 0, const.SENTINEL_2_RGB_CLIPPER).T * 255 / const.SENTINEL_2_RGB_CLIPPER,
+                    data[3],
                 ],
                 dtype='uint8',
             )
+            output = app_utils.get_image_from_pixels(pixpix, enhance=mode == 'S2')
 
-    output = BytesIO()
-    if return_empty:
-        path = os.path.dirname(os.path.abspath(__file__))
-        # Open the ref image.
-        img = Image.open(os.path.join(path, 'assets/tesselo_empty.png'))
-        # Write zoom message into image.
-        if z > 14:
-            msg = 'Zoom is {} | Max zoom is 14'.format(z)
-        else:
-            msg = 'No Data'
-        draw = ImageDraw.Draw(img)
-        text_width, text_height = draw.textsize(msg)
-        draw.text(((img.width - text_width) / 2, 60 + (img.height - text_height) / 2), msg, fill='black')
-        # Save image to io buffer.
-        img.save(output, format='PNG')
-    else:
-        # Create image object.
-        img = Image.fromarray(pixpix.T)
-        # Enhance color scheme for RGB mode.
-        if not formula and not sentinel_1:
-            img = ImageEnhance.Contrast(img).enhance(1.2)
-            img = ImageEnhance.Brightness(img).enhance(1.8)
-            img = ImageEnhance.Color(img).enhance(1.4)
-
-        # Save image to io buffer.
-        img.save(output, format=const.REQUEST_FORMAT_PNG)
-
-    output.seek(0)
     return send_file(
         output,
-        mimetype='image/png'
+        mimetype='image/{}'.format('png' if frmt == 'png' else 'tiff')
     )
 
 
