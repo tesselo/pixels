4383b2cda12f51f95e87e13303b769a15e6ccb78
Daniel Wiesmann <daniel@tesselo.com>
2019-07-03 11:26:53 +0100

    Uncommented tests.

--- tests/test_pixels.py
+++ tests/test_pixels.py
@@ -14,22 +14,22 @@ from tests.configs import gen_config, gen_configs
 @mock.patch('pixels.core.search.search', mock_functions.search)
 class TestPixels(unittest.TestCase):
 
-    # def test_pixels(self):
-    #     for config in gen_configs():
-    #         config = utils.validate_configuration(config)
-    #         core.handler(config)
-    #
-    # def test_timeseries(self):
-    #     config = gen_config({'interval': 'weeks', 'interval_step': 1})
-    #     config = utils.validate_configuration(config)
-    #     for here_start, here_end in utils.timeseries_steps(config['start'], config['end'], config['interval'], config['interval_step']):
-    #         # Update config with intermediate timestamps.
-    #         config.update({
-    #             'start': str(here_start.date()),
-    #             'end': str(here_end.date()),
-    #         })
-    #         # Trigger async task.
-    #         core.handler(config)
+    def test_pixels(self):
+        for config in gen_configs():
+            config = utils.validate_configuration(config)
+            core.handler(config)
+
+    def test_timeseries(self):
+        config = gen_config({'interval': 'weeks', 'interval_step': 1})
+        config = utils.validate_configuration(config)
+        for here_start, here_end in utils.timeseries_steps(config['start'], config['end'], config['interval'], config['interval_step']):
+            # Update config with intermediate timestamps.
+            config.update({
+                'start': str(here_start.date()),
+                'end': str(here_end.date()),
+            })
+            # Trigger async task.
+            core.handler(config)
 
     def test_algebra(self):
         height = width = 256
