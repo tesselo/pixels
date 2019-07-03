a79ca1cf61d290d4a100fd1f767c687a1d79efb2
Daniel Wiesmann <daniel@tesselo.com>
2019-07-03 16:36:08 +0100

    Added export batch scripts.

---
+++ batch/csv/export.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import io
+import json
+import logging
+import os
+import tempfile
+
+import boto3
+import geopandas
+from geodaisy import GeoObject
+
+from pixels import core, utils
+
+# General log setup.
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+# Get logger and set info level for this one.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Get path from env.
+project_id = os.environ.get('PROJECT_ID', 'pge_placer')
+filename = os.environ.get('GEO_FILE_NAME', 'pge_buff200_placer.gpkg')
+bucket = os.environ.get('AWS_S3_BUCKET', 'tesselo-pixels-results')
+tile_group_size = int(os.environ.get('TILE_GROUP_SIZE', 1000))
+
+# Get batch index from env.
+array_index = int(os.environ.get('AWS_BATCH_JOB_ARRAY_INDEX', 0))
+
+# Fetch geoms.
+dir = tempfile.mkdtemp()
+s3 = boto3.client('s3')
+src = os.path.join(dir, filename)
+s3.download_file(Bucket=bucket, Key=project_id + '/' + filename, Filename=src)
+
+#src = '/home/tam/Desktop/pge_buf200ft_ca/pge_buff200_placer.gpkg'
+
+# Open geographic file.
+pge_buf200_placer_df = geopandas.read_file(src)
+
+# Setup target files.
+header = None
+result = []
+
+# Loop through rows and call pixels.
+for index, row in pge_buf200_placer_df.iloc[(array_index * tile_group_size):((array_index + 1) * tile_group_size)].iterrows():
+    print('Working on feature ID', index)
+    config = {
+        'start': '2019-05-01',
+        'end': '2019-05-31',
+        'platform': 'Sentinel-2',
+        'product_type': 'S2MSI2A',
+        'max_cloud_cover_percentage': 90,
+        'mode': 'latest_pixel',
+        'color': False,
+        'format': 'CSV',
+        'delay': False,
+        'scale': 10,
+        'bands': [
+            'B04',
+            'B08',
+        ],
+        'formulas': [
+            {'name': 'NDVI', 'expression': '(B08 - B04) / (B08 + B04)'},
+        ],
+        'clip_to_geom': True,
+        'clip_all_touched': False,
+        'geom': {
+            'type': 'Feature',
+            'crs': pge_buf200_placer_df.crs['init'],
+            'properties': {
+                'line_segid': row['line_segid'],
+            },
+            'geometry': json.loads(GeoObject(row['geometry']).geojson()),
+        },
+    }
+
+    # Verify config.
+    config = utils.validate_configuration(config)
+    # Get pixels data as zip file.
+    output = core.handler(config)
+    # Convert to string by row.
+    data = output.read().decode().split('\n')
+    # Compare headers to ensure consistency.
+    if not header:
+        header = data[0]
+    elif header != data[0]:
+        raise ValueError('Inconsistent headers found in csv files.')
+    # Add data rows to result list, skipping header and empty last line.
+    result += data[1:-1]
+
+# Remove rows with nan NDVI values.
+result = [row for row in result if ',nan' not in row]
+
+# Combine all rows to one large file.
+result = '\n'.join([header] + result)
+
+#with open('/home/tam/Desktop/bla.csv', 'w') as fl:
+#    fl.write(result)
+
+# Upload result to bucket.
+with io.BytesIO(bytes(result, 'utf-8')) as output:
+    s3.put_object(
+        Bucket=bucket,
+        Key='{project_id}/result_{idx}.csv'.format(
+            project_id=project_id,
+            idx=array_index,
+        ),
+        Body=output,
+    )
---
+++ batch/csv/push_export.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+
+import logging
+import math
+import os
+import tempfile
+
+import boto3
+import geopandas
+
+# General log setup.
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+# Get logger and set info level for this one.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Get path from env.
+project_id = os.environ.get('PROJECT_ID', 'pge_placer')
+filename = os.environ.get('GEO_FILE_NAME', 'pge_buff200_placer.gpkg')
+bucket = os.environ.get('AWS_S3_BUCKET', 'tesselo-pixels-results')
+tile_group_size = int(os.environ.get('TILE_GROUP_SIZE', 1000))
+
+# Get batch index from env.
+array_index = int(os.environ.get('AWS_BATCH_JOB_ARRAY_INDEX', 0))
+
+# Fetch geoms.
+dir = tempfile.mkdtemp()
+s3 = boto3.client('s3')
+src = os.path.join(dir, filename)
+s3.download_file(Bucket=bucket, Key=project_id + '/' + filename, Filename=src)
+
+# Open
+pge_buf200_placer_df = geopandas.read_file(src)
+
+nr_of_geoms = len(pge_buf200_placer_df)
+
+batch_array_size = math.ceil(nr_of_geoms / tile_group_size)
+
+print('Found {} geoms - array size is {} - tile group size is {}'.format(nr_of_geoms, batch_array_size, tile_group_size))
+
+# Setup the job dict.
+job = {
+    'jobQueue': 'fetch-and-run-queue',
+    'jobDefinition': 'first-run-job-definition',
+    'containerOverrides': {
+        'environment': [
+            {'name': 'AWS_ACCESS_KEY_ID', 'value': os.environ.get('AWS_ACCESS_KEY_ID')},
+            {'name': 'AWS_SECRET_ACCESS_KEY', 'value': os.environ.get('AWS_SECRET_ACCESS_KEY')},
+            {'name': 'ESA_SCIHUB_USERNAME', 'value': os.environ.get('ESA_SCIHUB_USERNAME')},
+            {'name': 'ESA_SCIHUB_PASSWORD', 'value': os.environ.get('ESA_SCIHUB_PASSWORD')},
+            {'name': 'PROJECT_ID', 'value': project_id},
+            {'name': 'GEO_FILE_NAME', 'value': filename},
+            {'name': 'TILE_GROUP_SIZE', 'value': str(tile_group_size)},
+            {'name': 'AWS_S3_BUCKET', 'value': bucket},
+            {'name': 'BATCH_FILE_S3_URL', 'value': 's3://tesselo-pixels-scripts/batch.zip'},
+            {'name': 'BATCH_FILE_TYPE', 'value': 'zip'},
+            {'name': 'CURL_CA_BUNDLE', 'value': '/etc/ssl/certs/ca-certificates.crt'},
+        ],
+        'vcpus': 1,
+        'memory': 1024,
+    },
+    'retryStrategy': {
+        'attempts': 1
+    },
+}
+
+# Create client.
+batch = boto3.client('batch', region_name='eu-central-1')
+
+# Push training collection job.
+job['jobName'] = 'export-{}'.format(project_id)
+job['containerOverrides']['command'] = ['export.py']
+job['containerOverrides']['memory'] = 1024
+job['containerOverrides']['vcpus'] = 1
+
+if batch_array_size > 1:
+    job['arrayProperties'] = {'size': batch_array_size}
+
+current_job = batch.submit_job(**job)
+all_jobs = [current_job]
+
+{x['jobId']: x['status'] for x in batch.describe_jobs(jobs=[job['jobId'] for job in all_jobs])['jobs']}
