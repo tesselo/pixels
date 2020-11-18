b0a74d64b6995473f326dafc0f7b1a90b7194720
Daniel Wiesmann <daniel@tesselo.com>
2020-11-18 07:42:18 +0000

    Added latest pixel stack example to run mosaic.

--- tests/run_mosaic.py
+++ tests/run_mosaic.py
@@ -12,7 +12,7 @@ from fiona.transform import transform_geom
 from PIL import Image
 
 from pixels import search
-from pixels.mosaic import composite, latest_pixel_s2
+from pixels.mosaic import composite, latest_pixel_s2, latest_pixel_s2_stack
 from pixels.retrieve import retrieve
 from pixels.search import get_bands
 from pixels.utils import compute_wgs83_bbox, timeseries_steps
@@ -69,11 +69,22 @@ geojson = {
 
 # Get pixels.
 now = datetime.datetime.now()
-creation_args, stack = latest_pixel_s2(geojson, end_date='2020-10-31', scale=10, clip=True, bands=('B02', 'B03', 'B04', 'B08', 'B8A', 'B11', 'B12'), pool=True)
+# creation_args, stack = latest_pixel_s2(geojson, end_date='2020-10-31', scale=10, clip=True, bands=('B02', 'B03', 'B04', 'B08', 'B8A', 'B11', 'B12'), pool=True)
+result = latest_pixel_s2_stack(
+    geojson=geojson,
+    start='2020-10-01',
+    end='2020-10-31',
+    interval='months',
+    scale=10,
+    clip=True,
+    bands=('B02', 'B03', 'B04', 'B08', 'B8A', 'B11', 'B12'),
+    pool=True,
+)
+stack = result[0][1]
 print('Timing', datetime.datetime.now() - now)
 
 # Convert to image for visualization.
 img = numpy.dstack([255 * (numpy.clip(dat, 0, 4000) / 4000) for dat in [stack[2], stack[1], stack[0]]]).astype('uint8')    #Landsat case
 img = Image.fromarray(img)
 img.show()
-img.save('/home/keren/projects/API_Images/tests/1.png', 'PNG')
+# img.save('/home/keren/projects/API_Images/tests/1.png', 'PNG')
