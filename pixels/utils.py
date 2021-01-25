cf2fbf383cae252381b4bffd90abc4c79a5d0f0b
Daniel Wiesmann <daniel@tesselo.com>
2021-01-25 21:53:30 +0000

    Added docstrings for utils module functions.

--- pixels/utils.py
+++ pixels/utils.py
@@ -13,11 +13,37 @@ from rasterio.warp import transform
 
 def compute_transform(geojson, scale):
     """
-    Compute warp parameters from geometry. The scale is expected to be in the
-    coordinate system units of the geometry.
+    Compute raster transform parameters from geometry.
+
+    Computes the data necessary to create a raster based on an input geojson
+    dictionary. The geojson dictionary can have a non-standard CRS specified.
+    The scale argument is expected to be in the coordinate system units of the
+    geometry.
+
+
+    Parameters
+    ----------
+    geojson : dict
+        A geojson object that will be used for computing the bounding box.
+    scale : float or int
+        The scale for the output transform, in the CRS of the input geometry.
+
+    Returns
+    -------
+    transform : Affine
+        An affine transform for the bbox of the input geojson.
+
+    width : int
+        Pixel width for raster.
+
+    height : int
+        Pixel height for raster.
     """
+    # Compute bounding box of input geojson.
     extent = bounds(geojson)
+    # Compute the transform that defines the raster to create.
     transform = Affine(scale, 0, extent[0], 0, -scale, extent[3])
+    # Compute with and height of the raster to create.
     width = math.ceil((extent[2] - extent[0]) / scale)
     height = math.ceil((extent[3] - extent[1]) / scale)
 
@@ -28,8 +54,33 @@ def compute_mask(
     geojson, height, width, transform, value_column_name=None, all_touched=False
 ):
     """
-    Compute geometry mask. If specified, the burn value for each geometry will
-    be used from the value_column_name.
+    Burn a geometry into a raster.
+
+    Returns a numpy array with the rasterized version of the input geometries.
+    The burn value by default is a boolean. This can be made more fine grained
+    by specifying a column containing the burn value for each geometry.
+
+    Parameters
+    ----------
+    gejson : dict
+        Geometric data for rasterization.
+    height : int
+        Height of the mask array.
+    width : int
+        Width of the mask array.
+    transform : Affine
+        Transform for the rasterization.
+    value_column_name : str or None, optional
+        Name of the value attribute in the geojson that is used as burn value.
+    all_touched : bool or None, optional
+        Wether to use the all touched rasterization mode or not.
+
+
+    Returns
+    -------
+    mask : ndarray
+        The rasterized data, a boolean mask or a integer value from the value
+        column.
     """
     # Create a list of geometries, if requested with a burn value.
     if value_column_name:
@@ -59,6 +110,20 @@ def compute_mask(
 def compute_wgs83_bbox(geojson, return_bbox=False):
     """
     Computes the bounding box of the input geojson in WGS84 coordinates.
+
+    Parameters
+    ----------
+    gejson : dict
+        Geometric data from which to compute a bounding box.
+    return_bbox : bool or None, optional
+        If True, a simple bbox tuple is returned (xmin, ymin, xmax, ymax). If
+        False, the bbox is returned as geojson polygon feature.
+
+    Returns
+    -------
+    bbox : tuple or dict
+        The bounding box of the input geometry. Either as tuple or geojson
+        feature.
     """
     # Compute bounding box in original coordinates.
     bbox = bounds(geojson)
@@ -105,7 +170,30 @@ def compute_wgs83_bbox(geojson, return_bbox=False):
 
 def timeseries_steps(start, end, interval, intervals_per_step=1):
     """
-    Construct a series of timestep intervals given the input date range.
+    Construct a series of timestep intervals.
+
+    The intervals are from the start to the end date, in a fixed interval size.
+    Two dimensions define the intervals, the unit and the units per step. For
+    example, with a week interval and 2 steps by interval, the date ranges are
+    two weeks at a time.
+
+    Parameters
+    ----------
+    start : str
+        A parseable date or datetime string. Represents the start date of the
+        intervals. Example "2020-01-01".
+    end : str
+        A parseable date or datetime string. Represents the end date of the
+        intervals. Example "2020-12-31".
+    interval : str
+        A timestep interval. Needs to be interpretable by relativedelta. Valid
+        values include "years", "months", "weeks", and "days".
+
+    Returns
+    -------
+    intervals : list of tuples
+        A list of date tuples. Each tuple is of length two, representing the
+        start and end date of a single interval.
     """
     # Convert input to dates if provided as str.
     if isinstance(start, str):
@@ -130,7 +218,37 @@ def write_raster(
     data, args, out_path=None, driver="GTiff", dtype="float32", overviews=True, tags={}
 ):
     """
-    Given a numpy array and necessary metadata, save a image file.
+    Convert a numpy array into a raster object.
+
+    Given a numpy array and necessary metadata, create either an raster on disk
+    or return the raster in memory as a binary IO buffer. To create a file on
+    disk, provide an output path.
+
+    Parameters
+    ----------
+    data : array_like
+        The pixel values for the raster as numpy array.
+    args : dict
+        Rasterio creation arguments for the new raster.
+    out_path : str, optional
+        The path where the new file should be written on disk. If not provided,
+        a BytesIO object is returned with the raster in memory.
+    driver : str, optional
+        Rasterio driver for creating the new raster.
+    dtype : str, optional
+        Data type string specifying the output datatype.
+    overviews : bool, optional
+        Shall internal overviews be created for the new raster.
+    tags : dict, optional
+        A dictionary of tags to be added to the raster file. The namespace for
+        all tags will be "tesselo".
+
+    Returns
+    -------
+    raster : BytesIO or None
+        If an output path was provided, the raster is written on disk and None
+        is returned. Otherwise, the raster is returned as a binary blob in
+        memory.
     """
     # Set the raster metadata as the same as the input
     out_meta = args
