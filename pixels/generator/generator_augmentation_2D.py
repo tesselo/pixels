d3faef476c8093284e88b8e0e5910b7af9005c72
JotaFan <joao@tesselo.com>
2021-02-05 17:55:58 +0000

    Added various func to stac generator. Change behaviour in stac. #56. Closes #58.

--- pixels/generator/generator_augmentation_2D.py
+++ pixels/generator/generator_augmentation_2D.py
@@ -33,6 +33,31 @@ def set_standard_shape(tensor, sizex=360, sizey=360):
         tensor = set_standard_shape(tensor, sizex, sizey)
     return tensor
 
+def upscale_multiple_images(images_array, upscale_factor=10):
+    """
+    Upscale multiple images.
+
+    TODO: Decide if format is closed or open, if there is 2 loops or possible more.
+
+    Parameters
+    ----------
+        images_array : numpy array
+            List of images (Timestep, bands, img).
+        upscale_factor : int
+
+    Returns
+    -------
+        images_up : numpy array
+            List of images upscale by upscale_factor (Timestep, bands, img*upscale_factor).
+    """
+    new_array = []
+    for time in images_array:
+        new_time = []
+        for bands in time:
+            new_img = upscaling_sample(bands, upscale_factor)
+            new_time.append(np.array(new_img))
+        new_array.append(np.array(new_time))
+    return np.array(new_array)
 
 def img_flip(X, axis=None):
     X_f = np.flip(X, axis)
