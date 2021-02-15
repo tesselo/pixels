be0c01edc010614feffa8ac1e1bbac3b99d56fc0
JotaFan <joao@tesselo.com>
2021-02-15 20:10:40 +0000

    Added training from stac. Linter

--- pixels/stac_training.py
+++ pixels/stac_training.py
@@ -1,6 +1,9 @@
-from tensorflow.keras.models import model_from_json
-import pixels.stac_generator.generator_class as stcgen
+import json
+
 import pixels.stac as stc
+import pixels.stac_generator.generator_class as stcgen
+import tensorflow as tf
+
 
 def load_model_from_file(model_configuration_file):
     # Open config file and load as dict.
@@ -27,22 +30,26 @@ def compile_model_from_file(model, model_compile_arguments):
     model.compile(**input_config)
     return model
 
+
 def get_fit_args(model_fit_arguments_uri):
     # Open config file and load as dict.
-    if model_compile_arguments.startswith("s3"):
-        my_str = stc.open_file_from_s3(model_compile_arguments)["Body"].read()
+    if model_fit_arguments_uri.startswith("s3"):
+        my_str = stc.open_file_from_s3(model_fit_arguments_uri)["Body"].read()
         new_str = my_str.decode("utf-8")
         input_config = json.loads(new_str)
     else:
-        f = open(model_compile_arguments)
+        f = open(model_fit_arguments_uri)
         input_config = json.load(f)
     return input_config
 
-def train_model_function(catalog_uri, model_config_uri, model_compile_arguments_uri, model_fit_arguments_uri):
+
+def train_model_function(
+    catalog_uri, model_config_uri, model_compile_arguments_uri, model_fit_arguments_uri
+):
     dtgen = stcgen.DataGenerator_stac(catalog_uri, upsampling=10, timesteps=8)
     model = load_model_from_file(model_config_uri)
     model = compile_model_from_file(model, model_compile_arguments_uri)
     fit_args = get_fit_args(model_fit_arguments_uri)
     model.train(dtgen, **fit_args)
-    model.save(model_config_uri.replace('.txt, .model'))
+    model.save(model_config_uri.replace(".txt, .model"))
     return
