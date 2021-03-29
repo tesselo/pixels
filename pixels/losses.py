8aa1f76e666a7b53f2c6503cc7d48f27bb095f65
JotaFan <joao@tesselo.com>
2021-03-29 20:13:17 +0100

    Added working RMSE. Closes #157.

--- pixels/losses.py
+++ pixels/losses.py
@@ -10,7 +10,7 @@ def root_mean_squared_error(y_true, y_pred):
 def nan_mean_squared_error_loss(nan_value=np.nan):
     # Create a loss function
     def loss(y_true, y_pred):
-        #if y_true.shape != y_pred.shape:
+        # if y_true.shape != y_pred.shape:
         #    y_true = y_true[:, :1]
         indices = tf.where(tf.not_equal(y_true, nan_value))
         return tf.keras.losses.mean_squared_error(
