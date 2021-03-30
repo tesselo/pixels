3a8f377f1672bd0edfcd9cb07308d9a82e5d0e40
kerenvascs <kerenvasconcelos@gmail.com>
2021-03-30 20:08:47 +0100

    Changed method name in bands_combination.

--- pixels/bands_combinations.py
+++ pixels/bands_combinations.py
@@ -37,9 +37,9 @@ def get_index_bands(index_df, band_df, combination, satellite):
     return bands_dict
 
 #Test some examples for empty band case = L1-3
-#bands_dict = get_bands(indexes, bands, 'RGB', 'L1-3')
-#bands_dict = get_bands(indexes, bands, 'Bathymetric', 'L7')
-bands_dict = get_bands(indexes, bands, 'NBR', 'L4-L5-MSS')
+#bands_dict = get_index_bands(indexes, bands, 'RGB', 'L1-3')
+#bands_dict = get_index_bands(indexes, bands, 'Bathymetric', 'L7')
+bands_dict = get_index_bands(indexes, bands, 'NBR', 'L4-L5-MSS')
 bands_dict
 
 # Function to format formula using correct bands
@@ -71,25 +71,25 @@ def format_formula(df, bands_dict, combination):
     
     return formula
 
-# Test get_bands for all combinations
+# Test get_index_bands for all combinations
 comb_list = indexes.Combination.unique().tolist()
 satellites = ['S2', 'L8', 'L7', 'L4-L5-TM', 'L4-L5-MSS', 'L1-3']
 for c in comb_list:
     for s in satellites:
         comb = '{}'.format(c)
-        bands_dict = get_bands(indexes, bands, comb, s)
+        bands_dict = get_index_bands(indexes, bands, comb, s)
         print('{}'.format(c),'{}'.format(s), bands_dict)
 
 # Test format_formula for all combinations
 comb_list = indexes.Combination.unique().tolist()
 for c in comb_list:
     comb = '{}'.format(c)
-    bands_dict = get_bands(indexes, bands, comb, 'S2')
+    bands_dict = get_index_bands(indexes, bands, comb, 'S2')
     formula = format_formula(indexes, bands_dict, c)
     print('{}'.format(c), formula)
 
 #Test NDVI
-bands_dict = get_bands(indexes, bands, 'NDVI', 'S2')
+bands_dict = get_index_bands(indexes, bands, 'NDVI', 'S2')
 bands_dict
 #format_formula(indexes, bands_dict, 'NDVI')
 
