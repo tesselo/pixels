891b645b06497909934beeac7d702e3639948d0e
Daniel Wiesmann <daniel@tesselo.com>
2021-09-27 10:57:22 +0100

    feat(confusionmatrix): add more tests for the confusion matrix

--- tests/test_confusion_matrix.py
+++ tests/test_confusion_matrix.py
@@ -20,3 +20,28 @@ class ConfusionMatrixTest(unittest.TestCase):
             [0, 0, 0, 1],
         ]
         self.assertEqual(result, expected)
+
+    def test_confusion_matrix_reset_state(self):
+        mlcm = MultiLabelConfusionMatrix(4)
+        mlcm.update_state(
+            tf.one_hot([0, 1, 2, 3, 0, 0, 1, 1], 4),
+            tf.one_hot([0, 2, 1, 3, 0, 1, 0, 0], 4),
+        )
+        mlcm.reset_state()
+        result = mlcm.result().numpy().tolist()
+        expected = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(result, expected)
+
+    def test_confusion_matrix_get_config(self):
+        mlcm = MultiLabelConfusionMatrix(
+            4, dtype="uint8", name="Speedy Gonzalez Matrix"
+        )
+        self.assertEqual(
+            {"name": "Speedy Gonzalez Matrix", "dtype": "uint8", "num_classes": 4},
+            mlcm.get_config(),
+        )
