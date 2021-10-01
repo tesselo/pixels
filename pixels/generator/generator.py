fa9c715352269406943330db8a88d205ef4efca2
Joao <joao@tesselo.com>
2021-10-01 17:35:50 +0100

    fix(generator) instatiacion on class weights.

--- pixels/generator/generator.py
+++ pixels/generator/generator.py
@@ -118,9 +118,11 @@ class DataGenerator(keras.utils.Sequence):
         self.padding = padding
         self.y_zip = None
         if self.mode != GENERATOR_PIXEL_MODEL:
-            self.class_weights = [
-                f / sum(class_weights.values()) for f in class_weights.values()
-            ]
+            self.class_weights = class_weights
+            if self.class_weights:
+                self.class_weights = [
+                    f / sum(class_weights.values()) for f in class_weights.values()
+                ]
             self.x_open_shape = (
                 self.timesteps,
                 self.num_bands,
