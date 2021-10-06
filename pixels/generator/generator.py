9f634987b482084ec75a51c587cc18fc2ba050e9
Joao <joao@tesselo.com>
2021-10-06 16:21:27 +0100

    feat(generator) make downloads in pooling.

--- pixels/generator/generator.py
+++ pixels/generator/generator.py
@@ -197,11 +197,10 @@ class DataGenerator(keras.utils.Sequence):
                 os.path.dirname(self.path_collection_catalog), filetype="tif"
             )
             # Download the Pixels Data images in parallel.
-            with Pool(min(len(list_of_tifs), 50)) as p:
+            with Pool(min(len(list_of_tifs), 12)) as p:
                 p.starmap(
                     generator_utils.download_object_from_s3,
-                    list_of_tifs,
-                    [self.download_folder] * len(list_of_tifs),
+                    zip(list_of_tifs, [self.download_folder] * len(list_of_tifs)),
                 )
             # Retrieve path for training data.
             y_path_file = self.collection_catalog[
