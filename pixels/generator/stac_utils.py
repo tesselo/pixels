da5a9997c296871aee5706de47a0bda12e72c7f4
Daniel Moreno <dani@photonbit.es>
2022-02-09 17:45:35 +0000

    refactor(stac): simplify datetime usage

--- pixels/generator/stac_utils.py
+++ pixels/generator/stac_utils.py
@@ -219,7 +219,7 @@ def get_bbox_and_footprint_and_stats(raster_uri, categorical):
         Bounding box of input raster.
     footprint : list
         Footprint of input raster.
-    datetime_var : datetime type
+    datetime : datetime type
         Datetime from image.
     out_meta : rasterio meta type
         Metadata from raster.
@@ -245,9 +245,8 @@ def get_bbox_and_footprint_and_stats(raster_uri, categorical):
                 ]
             ],
         }
-        # Try getting the datetime in the raster metadata. Set to None if not
-        # found.
-        datetime_var = ds.tags().get("datetime", None)
+
+        datetime = ds.tags().get("datetime")
         # Compute unique counts if requested.
         stats = None
         if categorical:
@@ -256,4 +255,4 @@ def get_bbox_and_footprint_and_stats(raster_uri, categorical):
                 int(key): int(val) for key, val in zip(unique_values, uniue_counts)
             }
 
-        return bbox, footprint, datetime_var, ds.meta, stats
+        return bbox, footprint, datetime, ds.meta, stats
