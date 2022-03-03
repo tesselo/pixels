61dea066a3b323d3d77f99afe89130940c1f84a3
Joao <joao@tesselo.com>
2022-03-03 16:04:26 +0000

    feat(filters): added band index dictionary

--- pixels/generator/filters.py
+++ pixels/generator/filters.py
@@ -45,14 +45,26 @@ def _order_tensor_on_masks(image, mask_value, number_images=12):
     return np.array(image[ind])
 
 
-def order_tensor_on_cloud_mask(images, number_images):
-    B02 = images[:, 0]
-    B03 = images[:, 1]
-    B04 = images[:, 2]
-    B08 = images[:, 6]
-    B8A = images[:, 7]
-    B11 = images[:, 8]
-    B12 = images[:, 9]
+def order_tensor_on_cloud_mask(
+    images,
+    number_images,
+    bands_index={
+        "B02": 0,
+        "B03": 1,
+        "B04": 2,
+        "B08": 6,
+        "B8A": 7,
+        "B11": 8,
+        "B12": 9,
+    },
+):
+    B02 = images[:, bands_index["B02"]]
+    B03 = images[:, bands_index["B03"]]
+    B04 = images[:, bands_index["B04"]]
+    B08 = images[:, bands_index["B08"]]
+    B8A = images[:, bands_index["B8A"]]
+    B11 = images[:, bands_index["B11"]]
+    B12 = images[:, bands_index["B12"]]
     mask_img = _composite_or_cloud(
         B02,
         B03,
