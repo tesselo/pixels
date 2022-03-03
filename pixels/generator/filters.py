3c7b1942fec6106f4c43e56dad02a9cc925315f1
Joao <joao@tesselo.com>
2022-03-03 15:58:53 +0000

    feat(filters): added sorter on cloud cover

--- pixels/generator/filters.py
+++ pixels/generator/filters.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from pixels.clouds import _composite_or_cloud
+
 
 def _make_mask_on_value(img, mask_value):
     """
@@ -41,3 +43,29 @@ def _order_tensor_on_masks(image, mask_value, number_images=12):
     mask_count = np.sum(mask_img, axis=(1, 2, 3))
     ind = np.sort(np.argsort(mask_count)[:number_images])
     return np.array(image[ind])
+
+
+def order_tensor_on_cloud_mask(images, number_images):
+    B02 = images[:, 0]
+    B03 = images[:, 1]
+    B04 = images[:, 2]
+    B08 = images[:, 6]
+    B8A = images[:, 7]
+    B11 = images[:, 8]
+    B12 = images[:, 9]
+    mask_img = _composite_or_cloud(
+        B02,
+        B03,
+        B04,
+        B08,
+        B8A,
+        B11,
+        B12,
+        cloud_only=True,
+        light_clouds=False,
+        snow=True,
+        shadow_threshold=0.2,
+    )
+    mask_count = np.sum(mask_img, axis=(1, 2))
+    ind = np.sort(np.argsort(mask_count)[:number_images])
+    return np.array(images[ind])
