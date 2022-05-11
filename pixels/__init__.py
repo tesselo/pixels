604e021305e6444c84acf951fc7677a6c3850d64
Daniel Moreno <dani@photonbit.es>
2022-05-11 16:31:42 +0100

    refactor(logs): format all logs with structlog

--- pixels/__init__.py
+++ pixels/__init__.py
@@ -1,32 +1,13 @@
 import logging
 import os
-import sys
 
-import structlog
-from structlog_sentry import SentryJsonProcessor
+from kw.structlog_config import configure_stdlib_logging, configure_structlog
 
 __version__ = "0.1"
 
-# Set standard logging config.
-LOG_LEVEL = os.environ.get("LOG_LEVEL", logging.INFO)
-logging.basicConfig(format="%(message)s", stream=sys.stdout, level=LOG_LEVEL)
+DEBUG = os.environ.get("DEBUG", False)
+configure_structlog(debug=DEBUG, timestamp_format="iso")
+configure_stdlib_logging(debug=DEBUG, timestamp_format="iso", level=logging.INFO)
 
-# Set structlog logging config.
-structlog.configure(
-    processors=[
-        structlog.stdlib.filter_by_level,
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.stdlib.add_logger_name,
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
-        structlog.processors.StackInfoRenderer(),
-        structlog.processors.format_exc_info,
-        structlog.processors.UnicodeDecoder(),
-        SentryJsonProcessor(level=logging.ERROR, tag_keys="__all__"),
-        structlog.processors.JSONRenderer(),
-    ],
-    context_class=structlog.threadlocal.wrap_dict(dict),
-    logger_factory=structlog.stdlib.LoggerFactory(),
-    wrapper_class=structlog.stdlib.BoundLogger,
-    cache_logger_on_first_use=True,
-)
+# Stop the SPAM from botocore
+logging.getLogger("botocore.credentials").setLevel(logging.ERROR)
