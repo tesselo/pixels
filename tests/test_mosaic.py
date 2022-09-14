106bafbbd1540d61b3a8d728a3a1d262da0d91f3
Daniel Moreno <daniel.moreno@tesselo.com>
2022-09-14 17:54:22 +0100

    refactor(algebra): delete unused module

--- tests/test_mosaic.py
+++ tests/test_mosaic.py
@@ -5,7 +5,6 @@ from unittest import mock
 
 import numpy
 
-from pixels.algebra import parser
 from pixels.const import LANDSAT_1_LAUNCH_DATE
 from pixels.exceptions import PixelsException
 from pixels.mosaic import (
@@ -394,19 +393,3 @@ class TestMosaic(unittest.TestCase):
             + [[[7, 6, 7], [8, 5, 7], [5, 7, 5]]]
         ] * 4
         numpy.testing.assert_array_equal(stack, expected)
-
-    def test_algebra(self):
-        # Test regular latest pixel.
-        bands = ["B01", "B02"]
-        creation_args, first_end_date, stack = first_valid_pixel(
-            self.geojson,
-            end_date="2020-02-01",
-            scale=500,
-            bands=bands,
-            clip=False,
-            platforms="SENTINEL_2",
-            maxcloud=100,
-        )
-        ndvi = parser.evaluate("(B01 + 2 * B02) / (B01 + B02)", bands, stack)
-        expected = [[1.5, 1.5], [1.5, 1.5]]
-        numpy.testing.assert_array_equal(ndvi, expected)
