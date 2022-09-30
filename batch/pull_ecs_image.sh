235b95b451c9885cbee6834d4ca0edcb278d3b16
Daniel Wiesmann <daniel@tesselo.com>
2022-09-30 11:42:24 +0100

    fix(batch): add production tag to image pull script

--- batch/pull_ecs_image.sh
+++ batch/pull_ecs_image.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+TAG=${1:-"production"}
+
 echo "Logging in our private docker repository in AWS"
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 595064993071.dkr.ecr.eu-central-1.amazonaws.com
 
 echo "Pulling the image to our ECS repository"
-docker pull 595064993071.dkr.ecr.eu-central-1.amazonaws.com/tesselo-pixels:latest
+docker pull "595064993071.dkr.ecr.eu-central-1.amazonaws.com/tesselo-pixels:$TAG"
 
 echo "Done! :yay: you now have a copy of the latest image"
